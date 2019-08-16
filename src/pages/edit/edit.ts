58a55cf1db3e91306eb6d29fc2e0bb1505ba0258
ZaneleNkosi <50693705+ZaneleNkosi@users.noreply.github.com>
2019-08-16 17:05:31 +0200

    updates

--- src/pages/edit/edit.ts
+++ src/pages/edit/edit.ts
@@ -1,20 +1,12 @@
 import { ProfileProvider } from './../../providers/profile/profile';
 import { Component } from '@angular/core';
-import { Alert, AlertController, IonicPage, NavController, NavParams, LoadingController } from "ionic-angular"; 
-import { AuthProvider } from "../../providers/auth/auth"; 
+import { Alert, AlertController, IonicPage, NavController, NavParams, LoadingController } from "ionic-angular";
+import { AuthProvider } from "../../providers/auth/auth";
 import * as firebase from 'firebase';
 import { Profile } from '../../model/profile';
 import { InformationProvider } from '../../providers/information/information';
 import { ProfilePage } from '../profile/profile';
 import { Camera, CameraOptions } from '@ionic-native/camera';
-
-
-/**
- * Generated class for the EditPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -31,21 +23,20 @@ export class EditPage {
   showComp = true;
   profileImage: string;
   firstName?: string;
-  
- userProfile = {};
- 
+
+  userProfile = {};
+
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
-    public alertCtrl: AlertController, 
-    public authProvider: AuthProvider, 
-    public profileProvider: ProfileProvider ,
+    public alertCtrl: AlertController,
+    public authProvider: AuthProvider,
+    public profileProvider: ProfileProvider,
     private infoProv: InformationProvider,
     public camera: Camera,
     public loadingCtrl: LoadingController,
-    ) {
-      
-    }
+  ) {
+  }
 
   ionViewDidLoad() {
     this.db.collection('User Profiles').where('uid', '==', this.infoProv.returnUser().uid).get().then(snapshot => {
@@ -53,41 +44,39 @@ export class EditPage {
         this.userProfile = doc.data();
       })
       console.log(this.userProfile);
-      
-      
     })
-}
-
-async selectImage() {
-  const option: CameraOptions = {
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE,
-    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-    quality: 100
   }
-  await this.camera.getPicture(option).then(res => {
-    console.log('popover2', res);
 
-    let base64image = `data:image/jpeg;base64,${res}`;
-    this.profileImage = base64image;
-  })
-}
+  async selectImage() {
+    const option: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      quality: 100
+    }
+    await this.camera.getPicture(option).then(res => {
+      console.log('popover2', res);
 
-UpdatePro(userprofile){
-  let users = this.db.collection('User Profiles');
+      let base64image = `data:image/jpeg;base64,${res}`;
+      this.profileImage = base64image;
+    })
+  }
 
-  let load = this.loadingCtrl.create({
-    content: 'Loading'
-  });
+  UpdatePro(userprofile) {
+    let users = this.db.collection('User Profiles');
 
-  load.present();
+    let load = this.loadingCtrl.create({
+      content: 'Loading'
+    });
+
+    load.present();
     const images = this.storage.child(this.profile.bio + '.jpg')
     let upload = images.putString(this.profileImage, 'data_url');
 
     upload.on('state_changed', snapshot => {
       let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('popover2 Picture',progress);
+      console.log('popover2 Picture', progress);
 
     }, err => {
 
@@ -96,19 +85,19 @@ UpdatePro(userprofile){
         console.log('File available at: ', downURL);
         this.profile.image = downURL
 
-console.log(userprofile);
-  this.db.collection("User Profiles").doc(this.authProvider.getuser()).update(userprofile).then((ref) => {
-    load.dismiss();
-    console.log("This is the ref =",ref)
+        console.log(userprofile);
+        this.db.collection("User Profiles").doc(this.authProvider.getuser()).set(userprofile).then((ref) => {
+          load.dismiss();
+          console.log("This is the ref =", ref)
 
-  this.navCtrl.push(ProfilePage)
-});
-})
+          this.navCtrl.push(ProfilePage)
+        });
+      })
 
     })
   }
 
-  cancel(){
+  cancel() {
     this.navCtrl.push(ProfilePage);
   }
 }
